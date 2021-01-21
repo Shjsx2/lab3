e837852848ed27aa6ff8b9bb6f9c06d3e7df2f39
Shjsx2 <s4jin@ucsd.edu>
2021-01-21 00:44:02 -0800

    added animation to the thumbnails while clicking

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,21 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$(".jumbotron h1").text("connected");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$(".thumbnail").click(projectClick);
+	function projectClick(e){
+		var containingProject = $(this).closest(".project");
+		var description = $(containingProject).find(".project-description");
+		if (description.length == 0) {
+		   $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+		} else {
+			$(".project-description p").fadeToggle();
+		}
+	}
 }
